bc1c6ecc276abc5730a64edb6a74252d539b4358
pdf_jube_joba <61172336+pdf-jube-joba@users.noreply.github.com>
2022-02-24 14:51:37 +0900

    OK

--- connect.js
+++ connect.js
@@ -1,1 +1,36 @@
-var ta = document.getElementById("game");
+window.onload = function () {
+
+var table = document.getElementById('game')
+
+for (i = 0; i < height; i++) {
+  tr = document.createElement('tr')
+  for (j = 0; j < width; j++) {
+    td = document.createElement('td')
+    td.onclick = make_click(i,j)
+    tr.appendChild(td)
+  }
+  table.appendChild(tr)
+}
+
+function make_click(x,y){
+  return (function (){
+    if (reverse(x,y)){
+      update()
+    } else {
+      alert("負け")
+    }
+  })
+}
+
+function update(){
+  for (i = 0; i < height; i++){
+    for (j = 0; j < width; j ++){
+      if (T2[i][j] == 1){
+        table.rows[i].cells[j].innerHTML = count(i,j).toString()
+      } else {
+        table.rows[i].cells[j].innerHTML = " "
+      }
+    }
+  }
+}
+}
